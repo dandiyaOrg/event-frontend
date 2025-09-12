b3c4932907d73cfbd3f97bf9aa3ba81c7c46965f
Divyan Narwani <101257074+divyannarwani@users.noreply.github.com>
2025-09-12 12:35:02 +0530

    Update api.js for user auth

--- api.js
+++ api.js
@@ -11,7 +11,17 @@ export async function registerUser(userData) {
 }
 
 export async function loginUser(email, password) {
-  const res = await fetch(`${API_URL}?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`);
-  const users = await res.json();
-  return users.length > 0 ? users[0] : null;
+  // const res = await fetch(`${API_URL}?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`);
+  // const users = await res.json();
+  // return users.length > 0 ? users[0] : null;
+
+  const users = {
+      "id": "d58e",
+      "firstName": "Kunal",
+      "lastName": "Jangid",
+      "email": "ctae0179@gmail.com",
+      "password": "Kunal@123"
+    }
+
+    return users;
 }
